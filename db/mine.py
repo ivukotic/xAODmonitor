8fe10a1629080a4672fcf5ea022bd6688035e0b4
Ilija Vukotic <ivukotic@cern.ch>
2014-06-30 14:05:39 -0500

    update

--- db/mine.py
+++ db/mine.py
@@ -6,9 +6,36 @@ client = MongoClient('localhost', 27017)
 db=client.xAOD
 res = db.testData
 
+# res.remove({}) #all
+# res.remove({ 'cputime':{'$lt':100} })
+
 print 'rows:', res.count()
 #print 'data size:', res.dataSize()
 
+print '====================== first 2 rows'
 c=res.find().limit(2)
-for r in c:
-    print(r)
+for r in c: print(r)
+    
+print '====================== where cputime <200'
+c=res.find({ 'cputime':{'$lt':200} },{'cputime','walltime'})
+for r in c: print(r)
+
+print '====================== aggregating all.'        
+c=res.aggregate([
+                { '$match':{'cputime':{'$lt':2000}} },
+                { '$group':{ '_id':"$timestamp", 'totalCPU':{'$sum':"$cputime"}, 'totalWALL':{'$sum':"$walltime"} } } ])
+print c
+
+print '====================== map reduce.'      
+lastrun=1403800000;  
+c=res.mapReduce(
+    function(){ emit(this.timestamp,this.cputime); },
+    function(key,values){ return Array.sum(values) },
+    {
+        query:{timestamp:{'$gt':lastrun}},
+        out: 'skim'
+    }
+)
+print c
+c=db.skim.find()
+for r in c: print(r)
