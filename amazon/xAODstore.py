526d4446ff05a7a0620e1d82de9b8080bc3277de
Ilija Vukotic <ivukotic@cern.ch>
2014-07-17 15:32:17 -0500

    start testing amazon version.

---
+++ amazon/xAODstore.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import string
+import json as simplejson
+import cherrypy
+import time
+import json as simplejson
+
+class xAODreceiver(object):
+    exposed = True
+    @cherrypy.tools.accept(media='application/json')
+    def __init__(self):
+        logfile = open('xAODraw.txt', 'wb')
+        counter=0
+    def POST(self, data):
+        ts=int(time.time())
+        result=simplejson.JSONDecoder().decode(data)
+        result["timestamp"]=ts
+        logfile.write(result)
+        counter+=1
+        if counter>100:
+            logfile.close
+        return 'OK'
+        
+if __name__ == '__main__':    
+    cherrypy.config.update({'tools.log_headers.on': False})
+    print cherrypy.config
+    cherrypy.quickstart(xAODreceiver(), '/', '/home/ec2-user/xAODmonitor/server/xAODstore.conf')
+    
