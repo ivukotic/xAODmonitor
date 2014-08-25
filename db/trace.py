4472ab289f6d62407b6eb895fcafda1729594452
Ilija Vukotic <ivukotic@cern.ch>
2014-08-25 15:49:27 -0500

    trace update

--- db/trace.py
+++ db/trace.py
@@ -28,7 +28,7 @@ print 'rows:', res.count()
 print '====================== first 2 rows'
 c=res.find().limit(2)
 for r in c: print(r)
-    
+
 #print '====================== where cputime <200'
 #c=res.find({ 'cputime':{'$lt':200} },{'cputime':1,'walltime':1})
 #for r in c: print(r)
@@ -57,7 +57,7 @@ for r in c:
     #print r, hps
     ips=[]
     for i in hps:
-        ips.append(i[2])
+        ips.append(str(i[1]))
     ipsj=''.join(ips)
     #print r, ipsj
     phash = hashlib.md5(ipsj).hexdigest()
@@ -68,25 +68,35 @@ for r in c:
         break
 
 print "hashes added in: ",time.time()-a, "seconds"
-        
 
 #print '================ make sure there is an index on bhash.'
-#res.ensureIndex( { "bhash": 1 } )        
-        
-        
-#print '====================== map reduce. on bhash. sums of cputimes.'      
-#lastrun=1404156881
-#mapfunction = Code("function(){ emit(this.bhash,this.cputime) };")
-#reducefunction = Code("function(key,values){ return Array.sum(values) };")
-#c=res.map_reduce( mapfunction, reducefunction,'skim', query={'timestamp':{'$gt':lastrun}} )
-#print c
-#c=db.skim.find()
-#for r in c: print(r)
+#res.ensureIndex( { "phash": 1 } )        
 
+
+print '======================== distinct paths'
+dp=res.distinct("phash")
+print 'unique paths:',len(dp)
+        
+        
+print '====================== distinct ips'
+tps={}
+uips=Set()
+a=time.time()
+c=res.find({ "phash":{"$exists":True} })
+for r in c:
+    if c.phash in tps.keys: continue 
+    tps[c.phash]=[]
+    for ip in c.hops:
+        tps[c.phash]=ip
+        uips.add(ip)
+
+print len(tps)
+print len(uips)
 
 #print '====================== map reduce. on bhash. entries read per branch.'      
 #a=time.time()
 #lastrun=1404156881
 #que={'timestamp':{'$gt':lastrun}}
 #sor={'bhash': 1} 
+
 
