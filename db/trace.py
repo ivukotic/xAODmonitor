180970959f9235fa0c1a2b24631cfbeae2894bf8
Ilija Vukotic <ivukotic@cern.ch>
2014-08-25 17:49:01 -0500

    small update

--- db/trace.py
+++ db/trace.py
@@ -27,13 +27,7 @@ class IP:
         print "downstream:",self.downstream
         print "count:     ",self.counts
         
-def Int2IP(ipnum):
-    o1 = int(ipnum / 16777216) % 256
-    o2 = int(ipnum / 65536) % 256
-    o3 = int(ipnum / 256) % 256
-    o4 = int(ipnum) % 256
-    return '%(o1)s.%(o2)s.%(o3)s.%(o4)s' % locals()
-
+        
 
 client = MongoClient('localhost', 27017)
 db=client.trace
@@ -44,34 +38,12 @@ res = db.fax
 # print 'results removed:',r['n']
 
 print 'rows:', res.count()
-#while(True):a=res.count();time.sleep(10);print res.count()-a;
 #print 'data size:', res.dataSize()
 
 print '====================== first 2 rows'
 c=res.find().limit(2)
 for r in c: 
     print(r)
-    for i in r['hops']:
-        print Int2IP(i[1]),i[2]
-
-#print '====================== where cputime <200'
-#c=res.find({ 'cputime':{'$lt':200} },{'cputime':1,'walltime':1})
-#for r in c: print(r)
-
-#print '====================== aggregating all.'        
-#c=res.aggregate([
-#                { '$match':{'cputime':{'$lt':2000}} },
-#                { '$group':{ '_id':"$timestamp", 'totalCPU':{'$sum':"$cputime"}, 'totalWALL':{'$sum':"$walltime"} } } ])
-#print c
-
-#print '====================== map reduce. on time.'      
-#lastrun=1404156881;  
-#mapfunction = Code("function(){ emit(this.timestamp,this.cputime) };")
-#reducefunction = Code("function(key,values){ return Array.sum(values) };")
-#c=res.map_reduce( mapfunction, reducefunction,'skim', query={'timestamp':{'$gt':lastrun}} )
-#print c
-#c=db.skim.find()
-#for r in c: print(r)
 
 
 print '======================== adding hash manually'
@@ -93,10 +65,6 @@ for r in c:
         break
 
 print "hashes added in: ",time.time()-a, "seconds"
-
-#print '================ make sure there is an index on bhash.'
-#res.ensureIndex( { "phash": 1 } )        
-
 
 print '======================== distinct paths'
 distinctPhashes=res.distinct("phash")
@@ -133,10 +101,4 @@ for path in distinctPaths.values():
 for ip in distinctIPs.values():
     ip.prnt()
     
-#print '====================== map reduce. on bhash. entries read per branch.'      
-#a=time.time()
-#lastrun=1404156881
-#que={'timestamp':{'$gt':lastrun}}
-#sor={'bhash': 1} 
-
 
