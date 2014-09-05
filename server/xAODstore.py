d0b93b3cb0cbb7771f51a5d5d9e1bf68547628f0
Ilija Vukotic <ivukotic@cern.ch>
2014-09-04 19:31:25 -0500

    adding network

--- server/xAODstore.py
+++ server/xAODstore.py
@@ -5,6 +5,7 @@
 # cat d.json 
 # { "method" : "guru.test", "params" : [ "Guru" ], "id" : 123 }
 # curl -H "Accept: application/json" -X post "http://db.mwt2.org:8080/ips"
+# curl --data "source=MWT2&destination=AGLT2" -H "Accept: application/json" -X post "http://db.mwt2.org:8080/network"
 
 import random
 import string
@@ -23,6 +24,86 @@ tdb=client.trace
 tcollection = tdb.fax
 tnodes=tdb.nodes
 
+
+class IP:
+    def __init__(self,ip):
+        self.ip=ip
+        self.counts=0
+        self.upstream={}
+        self.downstream={}
+        self.name=""
+        self.longitude=0
+        self.latitude=0
+        self.countrycode=""
+        self.city=""
+        self.getDetails()
+    def getDetails(self):
+        try:
+            req = urllib2.Request("http://geoip.mwt2.org:4288/json/"+self.getIP(), None)
+            opener = urllib2.build_opener()
+            f = opener.open(req,timeout=5)
+            res=json.load(f)
+            # print res
+            self.longitude=res['longitude']
+            self.latitude=res['latitude']
+            self.countrycode=res['country_code']
+            self.city=res['city']
+        except:
+            print "# Can't determine client coordinates using geoip.mwt2.org ", sys.exc_info()[0]
+        try:
+            self.name=socket.gethostbyaddr(self.getIP())[0]
+        except socket.herror as e:
+            print "# Can't determine client name", e 
+    def addUpstream(self,ip):
+        if ip not in self.upstream:
+            self.upstream[ip]=1
+        else:
+            self.upstream[ip]+=1
+    def addDownstream(self,ip):
+        if ip not in self.downstream:
+            self.downstream[ip]=1
+        else:
+            self.downstream[ip]+=1
+    def getIP(self):
+        o1 = int(self.ip / 16777216) % 256
+        o2 = int(self.ip / 65536) % 256
+        o3 = int(self.ip / 256) % 256
+        o4 = int(self.ip) % 256
+        return '%(o1)s.%(o2)s.%(o3)s.%(o4)s' % locals()
+    def prnt(self):
+        print "name:      ",self.name, "\tIP:",self.getIP(), "\tlat/lon: ",self.longitude, self.latitude
+        print "country:   ",self.countrycode, "\tcity:",self.city
+        print "upstream:  ",self.upstream
+        print "downstream:",self.downstream
+        print "count:     ",self.counts
+
+
+class Network(object):
+    exposed = True
+    @cherrypy.tools.json_out()
+ 
+    def POST(self,source, destination):
+        # requ=cherrypy.request.json
+        rows=tcollection.find({"$and": [ {"from":source} , {"to":destination}, {"phash":{"$exists":True}} ] });
+        ret=[]
+        
+        distinctIPs={}
+        distinctPaths={}
+        for r in rows:
+            ph=r['phash']
+            if ph in distinctPaths.keys(): continue 
+            distinctPaths[ph]=[]
+            for h in r['hops']:
+                distinctPaths[ph].append(h[0])
+                if h[0] not in distinctIPs.keys():
+                    distinctIPs[h[0]] = IP(h[0])
+        
+        for n in distinctIPs.values():
+               ret.append({ "ip":n.ip, "name":n.name, "up":n.upstream, "down":n.downstream })
+               
+        return ret    
+
+
 class IPs(object):
     exposed = True
     @cherrypy.tools.json_out()
@@ -31,25 +112,23 @@ class IPs(object):
         # requ=cherrypy.request.json
         nods=tnodes.find()
         ret=[]
-        #for n in nods:
-	#    d=dumps(n)
-        #    ret.append(d)
-	for n in nods:
-	     upstream=[]
-	     downstream=[]
-             if n.has_key("upstream"): 
-	         up=n["upstream"]
-	     else:
-                 up=[]
-	     for i in up:  upstream.append([int(i),up[i]])
-	     if n.has_key("downstream"):
-	         down=n["downstream"]
-             else:
-                 down=[]
-	     for i in down:  downstream.append([int(i),down[i]])
-             lo=n["longitude"]
-	     la=n["latitude"]
-             ret.append({ "ip":n["ip"], "name":n["name"], "long":lo, "lat":la, "up":upstream, "down":downstream })
+        
+	    for n in nods:
+            upstream=[]
+            downstream=[]
+            if n.has_key("upstream"): 
+                up=n["upstream"]
+            else:
+                up=[]
+            for i in up:  upstream.append([int(i),up[i]])
+            if n.has_key("downstream"):
+                down=n["downstream"]
+            else:
+                down=[]
+            for i in down:  downstream.append([int(i),down[i]])
+            lo=n["longitude"]
+            la=n["latitude"]
+            ret.append({ "ip":n["ip"], "name":n["name"], "long":lo, "lat":la, "up":upstream, "down":downstream })
         return ret    
     
 class Trace(object):
@@ -67,6 +146,7 @@ class xAODreceiver(object):
     exposed = True
     trace=Trace()
     ips=IPs()
+    network=Network()
     
     @cherrypy.tools.accept(media='application/json')
     @cherrypy.tools.json_in()
