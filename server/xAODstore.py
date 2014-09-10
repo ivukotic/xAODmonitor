56a7b02db24eac2400feb2b7c954a7ac17c4344a
Ilija Vukotic <ivukotic@cern.ch>
2014-09-09 22:16:06 -0500

    second change to crow

--- server/xAODstore.py
+++ server/xAODstore.py
@@ -57,7 +57,7 @@ class BICdistincts(object):
         ret['Tasks']=rows.distinct("latest.ClusterId")
         ret['Users']=rows.distinct("latest.User")
         for r in range(len(ret['Users'])):
-            ret['Users'][r]=ret['Users'][r].split("@")[0])
+            ret['Users'][r]=ret['Users'][r].split("@")[0]
         print ret
         return ret
 
