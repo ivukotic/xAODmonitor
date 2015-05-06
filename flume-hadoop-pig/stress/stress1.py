cbac198b43364197d42efb0be8a6872369e783a3
Ilija Vukotic <ivukotic@cern.ch>
2015-05-06 09:06:10 -0500

    devs

---
+++ flume-hadoop-pig/stress/stress1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import urllib,urllib2
+import json as simplejson
+import random
+import time
+
+import socket
+timeout = 2
+socket.setdefaulttimeout(timeout)
+
+st=time.time()
+rows=10000
+
+for w in range(rows):
+    
+    result={}
+    result['cputime']=random.randint(0,1000)
+    result['walltime']=random.randint(0,3000)
+    result['files']=[]
+    result['branches']={}
+    for i in range(random.randint(1,10)):
+        result['files'].append("asdf.root")
+
+    for i in range(random.randint(990,998)):
+        result['branches']["branch_number_"+str(i)]=random.randint(0,1000)
+    
+    data=simplejson.JSONEncoder().encode(result)
+    try:
+        data = urllib.urlencode({'data':data})
+        req = urllib2.Request('http://hadoop-dev.mwt2.org:80/', data)
+        r = urllib2.urlopen(req)
+    except urllib2.HTTPError, err:
+        print err
+    
+
+    if (not w%10):
+        et=time.time()
+        print w,'insertion rate:',str(w/(et-st)),'Hz'
---
+++ flume-hadoop-pig/stress/tracer/setROOT.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Setting up gcc"
+source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/Gcc/current/setup.sh
+echo "Setting up ROOT"
+source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/root/5.34.07-x86_64-slc5-gcc4.3/bin/thisroot.sh
