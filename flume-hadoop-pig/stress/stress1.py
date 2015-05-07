4b83a18f47a645f6923b15de7dfb37141f948bf1
Ilija Vukotic <ivukotic@cern.ch>
2015-05-07 13:07:19 -0500

    devs

--- flume-hadoop-pig/stress/stress1.py
+++ flume-hadoop-pig/stress/stress1.py
@@ -17,7 +17,7 @@ for w in range(rows):
     
     event={}
     event['headers']={}
-    event['headers']['timestamp']=str(time.time()*10)
+    event['headers']['timestamp']=str(int(time.time()*1000))
     event['headers']['host']="random_host.example.com"
            
     result={}
