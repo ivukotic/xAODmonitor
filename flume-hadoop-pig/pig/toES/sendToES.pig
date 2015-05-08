6ab40a5623691a4f6881448f0100c985e1a74c2f
Ilija Vukotic <ivukotic@cern.ch>
2015-05-08 11:47:04 -0500

    devs

--- flume-hadoop-pig/pig/toES/sendToES.pig
+++ flume-hadoop-pig/pig/toES/sendToES.pig
@@ -5,15 +5,30 @@ REGISTER '/usr/lib/pig/lib/json-*.jar';
 REGISTER '/usr/lib/pig/lib/jython-*.jar';
 REGISTER '/usr/lib/pig/lib/snappy-*.jar';
 
+REGISTER '/home/ivukotic/xAODmonitor/elephant-bird-hadoop-compat-4.1.jar'
+REGISTER '/home/ivukotic/xAODmonitor/elephant-bird-pig-4.1.jar'
+
 REGISTER '/usr/lib/pig/lib/elasticsearch-hadoop-*.jar';
 
 define EsStorage org.elasticsearch.hadoop.pig.EsStorage('es.nodes=http://uct2-es-head.mwt2.org:9200');
 
--- RECS = LOAD 'xAODcollector' as (CRTIME: long,  PRODSOURCELABEL:chararray, times:bag{tuple(state:chararray, time:long)}, SORTED:int);
--- L = LIMIT RECS 100; dump L;
--- STORE L INTO 'xAOD-test/xAOD-test_record' USING EsStorage();
+--RECS = LOAD 'tests/data.json' using JsonLoader('cputime:int, walltime:int');
+
+REGISTER '/home/ivukotic/xAODmonitor/elephant-bird-hadoop-compat-4.1.jar'
+REGISTER '/home/ivukotic/xAODmonitor/elephant-bird-pig-4.1.jar'
+RECS = LOAD 'tests/data.13.1431022457749.json' using com.twitter.elephantbird.pig.load.JsonLoader('-nestedLoad');
+A = foreach RECS generate (int)$0#'cputime' as cputime, (int)$0#'walltime' as walltime, (bag{tuple(chararray)})$0#'files' as files, (map[])$0#'branches' as branches;
+describe A;
+
+RECS = LOAD 'tests/data1.json' using org.apache.pig.piggybank.storage.JsonLoader('files: {(fn:chararray)},cputime:int, walltime:int');
+RECS = LOAD 'tests/data1.json' using JsonLoader('files: {(fn:chararray)},cputime:int, walltime:int');
+RECS = LOAD 'xAODcollector' using JsonLoader('cputime:int, walltime:int');
+--RECS = LOAD 'tests/data.json' using JsonLoader('files: {(fn:chararray)}, branches: (br:chararray, reads:int),cputime:int,walltime:int');
+--files: {(fn:chararray)}, branches: (br:chararray, reads:int),
+-- times:bag{tuple(state:chararray, time:long)};
+L = LIMIT RECS 100; dump L;
+STORE L INTO 'xaodtest3/xaodtest_record' USING EsStorage();
 
 
-RECS = LOAD 'tests' USING PigStorage AS (F:int,S:int,T:int,N:chararray);
---dump RECS;
-STORE RECS INTO 'xAOD-test/xAOD-test_record' USING EsStorage();
+-- RECS = LOAD 'tests' USING PigStorage(',') AS (F:int,S:int,T:int,N:chararray);
+-- STORE RECS INTO 'xaodtest/xaodtest_record' USING EsStorage();
---
+++ flume-hadoop-pig/stress/tracer1/TFileAccessTracer.C
@@ -0,0 +1,119 @@
+#include <memory>
+#include <stdlib.h>
+#include <iostream>
+
+// ROOT include(s):
+#include <TSystem.h>
+#include <TFile.h>
+#include <TError.h>
+#include <TUrl.h>
+#include <TSocket.h>
+
+class aFiles(){
+    String filePath="/myPath/a/b/c/";
+    String fileName="myFilename.root";
+}
+
+using namespace std;
+int main(int argc, char **argv){
+    
+    static const char* SERVER_ADDRESS = "http://hadoop-dev.mwt2.org:18080";
+    static TUrl url( SERVER_ADDRESS );
+    TSocket socket( url.GetHost(), url.GetPort() );
+
+    if( ! socket.IsValid() ) {
+        cout<<"No valid socket could be established to" << endl;
+        return 1;
+    }
+
+    // Get some info about the user:
+    unique_ptr< UserGroup_t > uinfo( gSystem->GetUserInfo() );
+        
+        
+        
+        
+    // Start constructing the header of the message to send to the server:
+    TString hdr = "POST /";
+    hdr += url.GetFile();
+    hdr += " HTTP/1.0";
+    hdr += "\r\n";
+    hdr += "From: ";
+    hdr += gSystem->HostName();
+    hdr += "\r\n";
+    hdr += "User-Agent: xAODRootAccess\r\n";
+    hdr += "Content-Type: application/json\r\n";
+    hdr += "Content-Length: ";
+        
+    // Now construct the message payload:
+    
+    TString pld = "[{"
+    pld += "\"body\": \"{\\\"accessedFiles\\\": [";
+    bool first = true;
+    for( auto& info : m_accessedFiles ) {
+       if( ! first ) {
+          pld += ", ";
+       }
+       pld += "[\\\"" + info.filePath + "\\\",\\\"" + info.fileName + "\\\"]";
+       first = false;
+    }
+    pld += "],";
+    const xAOD::ReadStats& rs = xAOD::IOStats::instance().stats();
+    pld += " \\\"accessedContainers\\\": [";
+    first = true;
+    for( const auto& bs : rs.containers() ) {
+       if( ! bs.second.readEntries() ) {
+          continue;
+       }
+       if( ! first ) {
+          pld += ", ";
+       }
+       pld += "{\\\"name\\\": \\\"";
+       pld += bs.second.GetName();
+       pld += "\\\", \\\"readEntries\\\": ";
+       pld += bs.second.readEntries();
+       pld += "}";
+       first = false;
+    }
+    pld += "], ";
+    pld += " \\\"accessedBranches\\\": [";
+    first = true;
+    for( const auto& branch : rs.branches() ) {
+       for( const xAOD::BranchStats* bs : branch.second ) {
+          if( ( ! bs ) || ( ! bs->readEntries() ) ) {
+             continue;
+          }
+          if( ! first ) {
+             pld += ", ";
+          }
+          pld += "{\\\"name\\\": \\\"";
+          pld += bs->GetName();
+          pld += "\\\", \\\"readEntries\\\":";
+          pld += bs->readEntries();
+          pld += "}";
+          first = false;
+       }
+    }
+    pld += "]}, ";
+    pld += "\"headers\": {\"timestamp\": \"";
+    pld += TTimeStamp().GetSec();
+    pld += "\", \"host\": \"";
+    pld += gSystem->HostName();
+    pld += "\"}}]";
+    
+        
+    // Now finish constructing the header, and merge the two into a single message:
+    hdr += TString::Format( "%i", pld.Length() );
+    hdr += "\r\n\r\n";
+    const ::TString msg = hdr + pld;
+    
+    // A debug message for the time being:
+   	cout<< msg.Data()<<endl;
+   	
+   	// Send the message:
+   	if( socket.SendRaw( msg.Data(), msg.Length() ) < 0 ) {
+   	  cout<< "Failed to send message."<<endl;
+   	}
+   	   
+   	
+    return 0;    	
+}
